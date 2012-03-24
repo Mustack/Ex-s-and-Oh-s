346d422edcbe6532c35cf6c9355fdb2e796142f1
Jean-Luc <jl.martin1990@gmail.com>
2012-03-24 18:52:43 -0400

    fixed depency issue in views.py

--- tictactoe/app/views.py
+++ tictactoe/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from tictactoe.app.models import Player
 from django.contrib.auth import authenticate, login
 from django import forms
 
