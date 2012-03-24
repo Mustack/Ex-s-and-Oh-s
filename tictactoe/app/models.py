5a720cde713ce4b0f22e2b213abb29df5dfbcd49
Jean-Luc <jl.martin1990@gmail.com>
2012-03-24 19:08:39 -0400

    fixed user extended model

--- tictactoe/app/models.py
+++ tictactoe/app/models.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager
+from django.db import models
 
-class Player(models.User):
+class Player(models.Model):
     user = models.ForeignKey(User, unique=True)
     win_count = models.IntegerField()
