09ecaa9ac7fb5e6258b18b2220c78b4c5ee7c495
Jean-Luc <jl.martin1990@gmail.com>
2012-03-24 19:18:42 -0400

    fixed relation between Player and User

--- tictactoe/app/models.py
+++ tictactoe/app/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import User, UserManager
 from django.db import models
 
 class Player(models.Model):
-    user = models.ForeignKey(User, unique=True)
+    user = models.OneToOneField(User)
     name = models.CharField(max_length=15, primary_key=True)
     win_count = models.IntegerField()
